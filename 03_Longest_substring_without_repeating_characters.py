025442facc30d1665f3a880b601d956ce483e692
rameshpilli <ramesh3777@hotmail.com>
2021-08-14 17:06:15 -0400

    08_14_2021

---
+++ 03_Longest_substring_without_repeating_characters.py
@@ -0,0 +1,14 @@
+s = "abcabcbb"
+
+l = 0 
+d = set()
+res = 0
+for r in range(len(s)):
+    while s[r] in d:
+        d.remove(s[l])
+        l += 1
+    d.add(s[r])
+    res = max(res , r-l+1)
+
+#3
+print(res)
