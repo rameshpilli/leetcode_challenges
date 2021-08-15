4d490b92f7709afb23c732dad753feb8203e4f39
rameshpilli <ramesh3777@hotmail.com>
2021-08-14 20:53:27 -0400

    08_14_2021

--- 13_Roman_to_Integer.py
+++ 13_Roman_to_Integer.py
@@ -1,10 +1,9 @@
 letters = ["I","V","X","L","C","D","M"]           
 nums = [1,5,10,50,100,500,1000]
-
 lookup = dict(zip(letters, nums))
-
 s = "MCMXCIV"
 
+## approach 1
 i = len(s)-1                                                    ## last element
 res = 0     
 while  i >=0:
@@ -16,16 +15,16 @@ while  i >=0:
 
 print(res)
 
-## check if the previous value is <= current value
-## if less we sub 2(prev value)
+
+## approach 2
 prev = 0
 current = 0
 res = 0
 
 for i in range(len(s)):
     current = lookup[s[i]]
-    if prev < current:  
-        res = res + current - 2*prev
+    if prev < current:                                          ## check if the previous value is <= current value
+        res = res + current - 2*prev                            ## if less we sub 2(prev value)
     else:   
         res += current
     
