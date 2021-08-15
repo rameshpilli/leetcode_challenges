352cba6c08670774e595bb845f1617f0a32df4a7
rameshpilli <ramesh3777@hotmail.com>
2021-08-14 20:51:45 -0400

    08_14_2021

--- 13_Roman_to_Integer.py
+++ 13_Roman_to_Integer.py
@@ -7,7 +7,6 @@ s = "MCMXCIV"
 
 i = len(s)-1                                                    ## last element
 res = 0     
-
 while  i >=0:
     if i < len(s)-1 and lookup[s[i]] < lookup[s[i+1]]:          ## checking condition in reverse order, if I is before V or any other element we need to subtract 
         res -= lookup[s[i]]
@@ -15,4 +14,20 @@ while  i >=0:
         res = res + lookup[s[i]]
     i -= 1 
 
+print(res)
+
+## check if the previous value is <= current value
+## if less we sub 2(prev value)
+prev = 0
+current = 0
+res = 0
+
+for i in range(len(s)):
+    current = lookup[s[i]]
+    if prev < current:  
+        res = res + current - 2*prev
+    else:   
+        res += current
+    
+    prev = current
 print(res)
